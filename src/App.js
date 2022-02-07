cad2170172ef14c4891cef2005b594954c2fc99a
Jorge Nguiraze <jjorge@technoplus.co.mz>
2022-02-07 14:34:12 +0200

    forma base do layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,76 @@
 
 import './App.css';
+import React from "react";
+
 
 function App() {
   return (
-    <div>
-      <div className = "bg-orange-500 w-full ...">
-      Configuracao tailwindcss
-    </div>
+  
+   
+<div className="h-full w-full absolute bg-gray-200 justify-center">
+
+  
+           
+<div className="overlay flex flex-row items-center justify-center">
+        <div className="flex flex-row  lg:w-auto lg:flex-row">
+       
+        <div className="my-6 lg:my-12 container px-6 mx-auto flex flex-col lg:flex-row items-start lg:items-center justify-between pb-4 border-b border-gray-300">
+                <div className="container items-center ">
+                    <h1 className=" font-bold  justify-center text-black">VTTR</h1>
+                    <h6 className=" font-bold  text-black">Very Useful Tools to Remember</h6>
+                  
+                    <div className="flex justify-between py-1 px-5">
+                            <div className="flex flex-row space-x-4 items-center justify-center">
+                              <div className="flex border-2 rounded ">
+                                <div className="flex items-center justify-center px-4 border-l bg-white">
+                                <svg className="w-6 h-6 text-black " fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24">
+                                    <path
+                                    d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                                 </svg>
+                                </div>
+                              
+                                <input type="text" className="px-4 py-2 w-80" placeholder="Search..."/>
+                              
+                              </div>
+
+                              <div className="flex items-center justify-center">
+                                <label className="inline-flex items-center">
+                                <input type="checkbox" className="w-6 h-6 text-green-600 border-0 rounded-md focus:ring-0" />
+                                <span className="ml-2">Search in tags only</span>
+                                </label>
+                              </div>
+                                  <div className="flex items-center justify-end">
+                                 <button className="bg-white hover:bg-gray-700 text-black font-bold py-2 px-4 rounded">
+                                    Button
+                                 </button> 
+                                 </div>
 
 
 
-</div>
+
+                                                  
+                      </div>
+                   </div>
+                </div> 
+                 
+            </div>
+
+           
+      
+        </div>
+      </div>
+      <div className="overlay flex flex-row items-center justify-center">
+                  
+                  dsadadhjkjlk;jhghjklklkjhkjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+                  
+                  </div>
+</div> 
+
+
+
+        
+   
   );
 }
 
