67c32aaae577c6c4269eb0f11f0f13a7c71e632c
Jorge Nguiraze <jjorge@technoplus.co.mz>
2022-02-08 14:44:09 +0200

    Configuracao dos servicos necessarios para fazer o crud e pesquisa

---
+++ src/services/ToolService.js
@@ -0,0 +1,28 @@
+import http from "../http-common";
+const getAll = () => {
+    return http.get("/tools");
+};
+const get = id => {
+    return http.get('/tools/${id}');
+};
+const create = data => {
+    return http.post("/tools", data);
+};
+const update = (id, data) => {
+    return http.put('/tools/${id}', data);
+};
+const remove = id => {
+    return http.delete('/tools/${id}');
+};
+
+const findByTag = tag => {
+    return http.get('/tools?tag=${tag}');
+};
+export default {
+    getAll,
+    get,
+    create,
+    update,
+    remove,
+    findByTag
+};
